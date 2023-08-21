9f8b42863da9955202d4f8f24259c8d5ac9e975a
Yasmin Castillo <yasmin_c@outlook.com>
2023-08-21 11:52:52 -0500

    finished updating employee roles and adding employees

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const promptUser = () => {
                         })
                     })
             }
-            if(res.questions ==="Quit"){
+            if (res.questions === "Quit") {
                 process.exit();
             }
             if (res.questions === "Add Role") {
@@ -92,14 +92,84 @@ const promptUser = () => {
 
                         ])
                         .then(response => {
-                            connection.query("INSERT INTO roles(title,salary,department_id) VALUES(?,?,?)", [response.title,response.salary, response.department], (err, data) => {
+                            connection.query("INSERT INTO roles(title,salary,department_id) VALUES(?,?,?)", [response.title, response.salary, response.department], (err, data) => {
                                 console.table(data)
                                 promptUser();
                             })
                         })
                 })
             }
+            if (res.questions === "Add Employee") {
+                connection.query('SELECT * FROM roles', (err, data) => {
+                    const roles = data.map(role => ({ name: role.title, value: role.id }))
+                    connection.query('SELECT * FROM employees', (err, data) => {
+                        const employees = data.map(employee => ({ name: employee.first_name + " "+ employee.last_name, value: employee.id }))
+                        inquirer
+                            .prompt([{
+                                type: "input",
+                                name: "first",
+                                message: "What is the employee's first name?"
+                            },
+                            {
+                                type: "input",
+                                name: "last",
+                                message: "What is the employee's last name"
+                            },
+                            {
+                                type: "list",
+                                name: "role",
+                                message: "What role does this employee belong to?",
+                                choices: roles
+                            },
+                            {
+                                type: "list",
+                                name: "manager",
+                                message: "What manager does this employee respond to?",
+                                choices: employees
+                            }
 
+                            ])
+                            .then(response => {
+                                connection.query("INSERT INTO employees(first_name,last_name,role_id,manager_id) VALUES(?,?,?,?)", [response.first, response.last, response.role,response.manager], (err, data) => {
+                                    console.table(data)
+                                    promptUser();
+                                })
+                            })
+                    })
+                })
+            }
+
+            if (res.questions === "Update Employee Role") {
+                connection.query('SELECT * FROM roles', (err, data) => {
+                    const roles = data.map(role => ({ name: role.title, value: role.id }))
+                    connection.query('SELECT * FROM employees', (err, data) => {
+                        const employees = data.map(employee => ({ name: employee.first_name + " "+ employee.last_name, value: employee.id }))
+                        inquirer
+                            .prompt([
+                            
+                            {
+                                type: "list",
+                                name: "employee",
+                                message: "What employee needs to update their role?",
+                                choices: employees
+                            },
+                            {
+                                type: "list",
+                                name: "role",
+                                message: "What role do you want to change it to?",
+                                choices: roles
+                            }
+
+                            ])
+                            .then(response => {
+                                connection.query("UPDATE employees SET role_id = ? WHERE id = ?", [response.role, response.employee], (err, data) => {
+                                    console.table(data)
+                                    promptUser();
+                                })
+                            })
+                    })
+                })
+            }
 
 
         })
